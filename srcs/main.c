1e17d15fa8846b804f206ac5ed8f339e68ebf14e
Florian Lombardo <flombard@z4r7p3.le-101.fr>
2018-09-21 04:31:57 +0200

    ajout des textures sur les murs, non optimise mais fonctionnel

--- srcs/main.c
+++ srcs/main.c
@@ -29,6 +29,12 @@ static int		win_init(t_env *e)
 		dprintf(1, "SDL_CreateWindowAndRenderer() error: %s\n", SDL_GetError());
 		return (0);
 	}
+	if (!(e->tex = SDL_CreateTexture(e->rend, SDL_PIXELFORMAT_RGBA8888,
+		SDL_TEXTUREACCESS_TARGET, WID, HEI)))
+	{
+		dprintf(1, "SDL_CreateTexture() error: %s\n", SDL_GetError());
+		return (0);
+	}
 	SDL_SetWindowTitle(e->win, "Wolf3d");
 	if (!(e->icon = IMG_Load("textures/icon.png")))
 	{
@@ -36,10 +42,9 @@ static int		win_init(t_env *e)
 		return (0);
 	}
 	SDL_SetWindowIcon(e->win, e->icon);
-	if (!(e->tex = SDL_CreateTexture(e->rend, SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_TARGET, WID, HEI)))
+	if (!(e->wall = IMG_Load("textures/wall.png")))
 	{
-		dprintf(1, "SDL_CreateTexture() error: %s\n", SDL_GetError());
+		dprintf(1, "IMG_Load() error: %s\n", IMG_GetError());
 		return (0);
 	}
 	return (1);
@@ -47,6 +52,8 @@ static int		win_init(t_env *e)
 
 static void		quit(t_env *e, int status)
 {
+	if (e->wall)
+		SDL_FreeSurface(e->wall);
 	if (e->icon)
 		SDL_FreeSurface(e->icon);
 	if (e->tex)
@@ -65,31 +72,31 @@ static void		deal_key(t_env *e, SDL_Event event)
 	double	oldplane;
 	double	olddir;
 
-	if (event.key.keysym.sym == SDLK_a && event.key.state == SDL_PRESSED && event.key.repeat == 0)
+	if (event.key.keysym.sym == SDLK_a && event.key.state == SDL_PRESSED)
 	{
 		olddir = e->p.dir.x;
-		e->p.dir.x = e->p.dir.x * cos(0.1) - e->p.dir.y * sin(0.1);
-		e->p.dir.y = olddir * sin(0.1) + e->p.dir.y * cos(0.1);
+		e->p.dir.x = e->p.dir.x * COS - e->p.dir.y * PSIN;
+		e->p.dir.y = olddir * PSIN + e->p.dir.y * COS;
 		oldplane = e->p.plane.x;
-		e->p.plane.x = e->p.plane.x * cos(0.1) - e->p.plane.y * sin(0.1);
-		e->p.plane.y = oldplane * sin(0.1) + e->p.plane.y * cos(0.1);
+		e->p.plane.x = e->p.plane.x * COS - e->p.plane.y * PSIN;
+		e->p.plane.y = oldplane * PSIN + e->p.plane.y * COS;
 		if (SDL_RenderClear(e->rend))
 			quit(e, 1);
 		raycasting(e);
 	}
-	else if (event.key.keysym.sym == SDLK_d && event.key.state == SDL_PRESSED && event.key.repeat == 0)
+	else if (event.key.keysym.sym == SDLK_d && event.key.state == SDL_PRESSED)
 	{
 		olddir = e->p.dir.x;
-		e->p.dir.x = e->p.dir.x * cos(-0.1) - e->p.dir.y * sin(-0.1);
-		e->p.dir.y = olddir * sin(-0.1) + e->p.dir.y * cos(-0.1);
+		e->p.dir.x = e->p.dir.x * COS - e->p.dir.y * NSIN;
+		e->p.dir.y = olddir * NSIN + e->p.dir.y * COS;
 		oldplane = e->p.plane.x;
-		e->p.plane.x = e->p.plane.x * cos(-0.1) - e->p.plane.y * sin(-0.1);
-		e->p.plane.y = oldplane * sin(-0.1) + e->p.plane.y * cos(-0.1);
+		e->p.plane.x = e->p.plane.x * COS - e->p.plane.y * NSIN;
+		e->p.plane.y = oldplane * NSIN + e->p.plane.y * COS;
 		if (SDL_RenderClear(e->rend))
 			quit(e, 1);
 		raycasting(e);
 	}
-	else if (event.key.keysym.sym == SDLK_w && event.key.state == SDL_PRESSED && event.key.repeat == 0)
+	else if (event.key.keysym.sym == SDLK_w && event.key.state == SDL_PRESSED)
 	{
 		if (!map[(int)e->p.pos.y][(int)(e->p.pos.x + e->p.dir.x * 0.3)])
 			e->p.pos.x +=e->p.dir.x * 0.3;
@@ -99,7 +106,7 @@ static void		deal_key(t_env *e, SDL_Event event)
 			quit(e, 1);
 		raycasting(e);
 	}
-	else if (event.key.keysym.sym == SDLK_s && event.key.state == SDL_PRESSED && event.key.repeat == 0)
+	else if (event.key.keysym.sym == SDLK_s && event.key.state == SDL_PRESSED)
 	{
 		if (!map[(int)e->p.pos.y][(int)(e->p.pos.x - e->p.dir.x * 0.3)])
 			e->p.pos.x -= e->p.dir.x * 0.3;
@@ -128,6 +135,7 @@ void			raycasting(t_env *e)
 {
 	double	camx;
 	double	dist;
+	double	wallx;
 	t_dpos	raydir;
 	t_dpos	delta;
 	t_dpos	sidedist;
@@ -140,8 +148,11 @@ void			raycasting(t_env *e)
 	int		height;
 	int		start;
 	int		end;
+	Uint32	*pixels;
+	Uint32	pixel;
 
 	SDL_SetRenderTarget(e->rend, e->tex);
+	pixels = e->wall->pixels;
 	x = -1;
 	while (++x < WID)
 	{
@@ -197,18 +208,28 @@ void			raycasting(t_env *e)
 			start = 0;
 		if ((end = height / 2 + HEI / 2) >= HEI)
 			end = HEI - 1;
+		wallx = (side) ? e->p.pos.x + dist * raydir.x : e->p.pos.y + dist * raydir.y;
+		wallx -= floor(wallx);
 		y = start - 1;
-		if (!side)
+		/*if (!side)
 			SDL_SetRenderDrawColor(e->rend, 0, 0, 255, 255);
 		else
-			SDL_SetRenderDrawColor(e->rend, 0, 0, 136, 255);
+			SDL_SetRenderDrawColor(e->rend, 0, 0, 136, 255);*/
 		while (++y < end)
-			SDL_SetRenderDrawPoint(e->rend, x, y);
-		SDL_SetRenderDrawColor(169, 169, 169, 255);
-		while (++y < HEI)
 		{
-			SDL_SetRenderDrawPoint(e->rend, x, y);
-			SDL_SetRenderDrawPoint(e->rens, x, HEI - y - 1);
+			SDL_Color	c;
+			pixel = pixels[(int)(((double)(y - start) / (double)(end - start)) * 64) * e->wall->w + (int)(wallx * 64)];
+			//if (x == 400) dprintf(1, "%zu\n", (size_t)pixel);
+			SDL_GetRGBA(pixel, e->wall->format, &c.r, &c.g, &c.b, &c.a);
+			SDL_SetRenderDrawColor(e->rend, c.r, c.g, c.b, c.a);
+			SDL_RenderDrawPoint(e->rend, x, y);
+		}
+		SDL_SetRenderDrawColor(e->rend, 169, 169, 169, 255);
+		while (y < HEI)
+		{
+			SDL_RenderDrawPoint(e->rend, x, y);
+			SDL_RenderDrawPoint(e->rend, x, HEI - 1 - y);
+			y++;
 		}
 	}
 	SDL_SetRenderTarget(e->rend, NULL);
@@ -230,9 +251,9 @@ int				main(void)
 	while (!b)
 	{
 		SDL_WaitEvent(&event);
-		deal_key(&e, event);
 		if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
 			b = SDL_TRUE;
+		deal_key(&e, event);
 	}
 	quit(&e, 0);
 	return (EXIT_SUCCESS);
